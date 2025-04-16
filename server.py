d68a682eb8cab6da8059826376605f8e015a9fa1
Glitch (hello-flask) <none>
2025-04-16 11:08:38 +0000

    🐴⏰ Checkpoint ./server.py:65813575/210

--- server.py
+++ server.py
@@ -1,7 +1,10 @@
-from flask import flask
+from flask import Flask, render_template
 
-app = flask(__name__)
+app = Flask(__name__)
 
 @app.route('/')
 def welcome():
     return render_template('index.html')
+  
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=3000, debug=True)
