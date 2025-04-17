e89272169f2533958ee9d44cceeb5da1a693d6a3
Glitch (hello-flask) <none>
2025-04-17 12:38:06 +0000

    ☔️💗 Checkpoint ./main.py:65813575/3060

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, session, redirect, flash
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['POST', 'GET'])
 def welcome():
+  if 'user' in session and session['user'] == params['admin_user']:
+        if request.method == 'POST':
+            return render_template('main.html')
+        return render_template('main.html')
+    elif request.method == 'POST':
+        username = request.form.get('email')
+        password = request.form.get('pass')
+        if username == params['admin_user'] and password == params['admin_password']:
+            session['user'] = username
+            return render_template('main.html')
     return render_template('index.html')
   
 if __name__ == '__main__':
