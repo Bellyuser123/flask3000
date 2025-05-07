bf3374563d3241df8578189aff2647e7afb16b07
Glitch (hello-flask) <none>
2025-05-07 15:11:33 +0000

    🎢🏗 Checkpoint ./main.py:65813575/9

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ def welcome():
     if username == admin_user and password == admin_password:
       session['user'] = username
       return render_template('main.html')
-  return render_template('index.html')
+  return render_template('main.html')
   
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=3000, debug=True)
