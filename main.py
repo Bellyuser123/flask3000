7f69be0336eb59b3f854b7605f2f8e2b24f69c9a
Glitch (hello-flask) <none>
2025-06-28 10:45:17 +0000

    🌊🎁 Checkpoint ./main.py:65813575/1317

--- main.py
+++ main.py
@@ -4,11 +4,32 @@ from sqlalchemy import DateTime
 import os
 
 
+local_server = True
+
 app = Flask(__name__)
 
 app.secret_key = os.getenv('key')
 admin_user = os.getenv('admin_user')
 admin_password = os.getenv('admin_password')
+
+
+base_dir = os.path.abspath(os.path.dirname(__file__))
+if local_server:
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('local_uri') + os.path.join(base_dir, 'data', 'database.db')
+else:
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('prod_uri') + os.path.join(base_dir, 'data', 'database.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+
+class Contacts(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False)
+    email = db.Column(db.String(120), nullable=False)
+    message = db.Column(db.Text, nullable=False)
+    date = db.Column(DateTime)
+
 
 
 @app.route('/', methods=['POST', 'GET'])
@@ -23,7 +44,13 @@ def welcome():
     if username == admin_user and password == admin_password:
       session['user'] = username
       return render_template('main.html')
-  return render_template('index.html')
+  return render_template('index.html
+                         
+                         
+@app.route('/admin_panel', methods=['POST', 'GET'])
+def admin_panel_main():
+    if 'user' in session and session['user'] == admin_user:
+        return render_template('')
 
 
 @app.route("/logout")
@@ -33,4 +60,6 @@ def logout():
 
 
 if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
     app.run(host='0.0.0.0', port=3000, debug=True)
