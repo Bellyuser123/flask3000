06f35bdd3fadee7cd7072f1b8659e0af3c867cef
Glitch (hello-flask) <none>
2025-07-04 09:54:28 +0000

    🌯👵 Checkpoint ./templates/edit_m.html:65813575/115 ./main.py:65813575/303

--- main.py
+++ main.py
@@ -254,13 +254,15 @@ def editing_sec(id, type):
                 phone = request.form.get('phone')
                 email = request.form.get('email')
                 blood = request.form.get('blood')
+                name=f"{ln} {fn} {mn}"
+                father=f"{fln} {ffn} {fmn}"
                 
                 id_list = id.split("?")
                 family_id = request.args.get('family_id', type=int)
                 print(family_id)
                 
                 if not id_list[0] or id_list[0] == 'new':
-                    mem = Member(id=None, family_id=family_id, name=f"{ln} {fn} {mn}", father=f"{fln} {ffn} {fmn}", gender=gender, relation=relation, dob=dob, photo=photo, peear=peear, marriage=marriage, edu=edu, occu=occu, email=email, phone=phone, blood=blood)
+                    mem = Member(id=None, family_id=family_id, name=name, father=father, gender=gender, relation=relation, dob=dob, photo=photo, peear=peear, marriage=marriage, edu=edu, occu=occu, email=email, phone=phone, blood=blood)
                     fam = Family.query.filter_by(id=family_id).first()
                     fam.mem_num += 1
                     db.session.add(mem)
@@ -281,7 +283,7 @@ def editing_sec(id, type):
                         mem.phone = phone 
                         mem.email = email 
                         mem.blood = blood 
-                        mem.family_id = family_id 
+                        family_id = mem.family_id
                         
                         db.session.commit()
                         return redirect('/summary/' + str(family_id))
